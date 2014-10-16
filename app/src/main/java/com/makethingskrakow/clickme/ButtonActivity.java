a11757d260c810040bd50291422d2413e0f453c6
Bartek Filipowicz <bartek.f@getbase.com>
2014-10-16 21:49:50 +0200

    Add examples of button actions

--- app/src/main/java/com/makethingskrakow/clickme/ButtonActivity.java
+++ app/src/main/java/com/makethingskrakow/clickme/ButtonActivity.java
@@ -20,5 +20,17 @@ public class ButtonActivity extends Activity implements OnClickListener {
   @Override
   public void onClick(View button) {
     // PUT YOUR ACTIONS HERE
+
+    // Uncomment this to change background color to green after clicking on the button
+    // new ChangeBackgroundColor(0xff00ff00).perform(getWindow().getDecorView());
+
+    // Uncomment this to change color of a button to random one after clicking on it
+    // new ChangeBackgroundColor(new RandomColorGenerator().getRandomColor()).perform(button);
+
+    // Uncomment this to change show "Hello world!" message after clicking the button
+    // new ShowTextAction("Hello world!").perform(this);
+
+    // Uncomment this to play a "bazinga" sound after clicking the button
+    // new PlaySoundAction(R.raw.bazinga, this).play();
   }
 }
